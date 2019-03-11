ed80cbc7e70460292ffe3ebda53b1bd55c7601e6
cyyzero <cyyzero16@qq.com>
2019-03-11 23:58:30 +0800

    Update tss.

--- src/userprog/tss.c
+++ src/userprog/tss.c
@@ -45,11 +45,37 @@ void update_tss_esp(struct task_struct* pthread)
     tss.esp0 = (uint32_t*)((uint32_t)pthread + PG_SIZE);
 }
 
-static struct gdt_desc make_gdt_desc(uint32_t* desc_addr, uint32_t limit, uint8_t addr_low, uint8_t attr_high)
+// 创建gdt描述符
+void make_gdt_desc(struct gdt_desc* desc, uint32_t desc_addr, uint32_t limit, uint8_t attr_low, uint8_t attr_high)
 {
-    uint32_t desc_base = (uint32_t)desc_addr;
-    struct gdt_desc desc;
-    desc.limit_low_word = limit & 0x0000ffff;
-    desc.base_low_word  = desc_base & 0x0000ffff;
-    desc.base_mid_byte  = (desc_base & 0x00ff0000) >> 16;
+    desc->limit_low_word = limit & 0x0000ffff;
+    desc->base_low_word  = desc_base & 0x0000ffff;
+    desc->base_mid_byte  = (desc_base & 0x00ff0000) >> 16;
+    desc->attr_low_byte  = attr_low;
+    desc->limit_high_attr_high = ((limit & 0x000f0000) >> 16) + (uint8_t)attr_high;
+    desc->base_high_byte = desc_base >> 24;
 }
+
+// 在gdt中创建tss并重新加载gdt
+void tss_init(void)
+{
+    put_str("tss_init start\n");
+
+    uint32_t tss_size = sizeof(tss);
+    memset(&tss, 0, tss_size);
+    tss.ss0 = SELECTOR_K_STACK;
+    tss.io_base = tss_size;
+
+    // gdt段基质为0x900，把tss放在第四个位置，既0x900+0x20
+
+    make_gdt_desc((struct gdt_desc*)0xc0000920, (uint32_t)&tss, tss_size-1, TSS_ATTR_LOW, TSS_ATTR_HIGH);
+    make_gdt_desc((struct gdt_desc*)0xc0000928, 0, 0xfffff, GDT_CODE_ATTR_LOW_DPL3, GDT_ATTR_HIGH);
+    make_gdt_desc((struct gdt_desc*)0xc0000930, 0, 0xfffff, GDT_CODE_ATTR_LOW_DPL3, GDT_ATTR_HIGH);
+
+    uint32_t gdt_operand = ((8 * 7 - 1) | ((uint32_t)0xc0000900 << 16));
+    asm volatile ("lgdt %0" : : "m" (gdt_operand));
+    asm volatile ("ltr %w0" : : "r" (SELECTOR_TSS));
+    put_str("tss_init and ltr done\n");
+}
+
+
