872ee591e831963761d1260e4dfd7cd8b22023b5
cyyzero <cyyzero16@qq.com>
2019-05-26 22:07:16 +0800

    Fix a bug, left shift overflow

--- src/kernel/interrupt.c
+++ src/kernel/interrupt.c
@@ -203,7 +203,7 @@ void idt_init()
     pic_init();                              // 初始化8259A
 
     // 加载IDT
-    uint64_t idt_operand = (sizeof(idt) - 1) | ((uint64_t)((uint32_t)idt << 16));
+    uint64_t idt_operand = (sizeof(idt) - 1) | ((uint64_t)idt << 16);
     asm volatile("lidt %0" : : "m" (idt_operand));
     put_str("idt_init done\n");
 }
